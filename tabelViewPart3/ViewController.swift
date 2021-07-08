39eb57f58bb7d7f427f6f9a7048e76e909517961
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-08 22:02:20 +0900

    UITextFieldDelegateを使ってTextFieldに入力された値を配列に入れてキーボードのリターンが押された時にセルに追加するように実装してみた

--- tabelViewPart3/ViewController.swift
+++ tabelViewPart3/ViewController.swift
@@ -7,43 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource , UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource , UITableViewDelegate, UITextFieldDelegate {
    
 
     @IBOutlet weak var tableView: UITableView!
-    var items = [String]() //String型の空配列の準備
     var count = 1
     @IBOutlet weak var label: UILabel!
+    
+    
+    var todos = [String]()     //String型の空配列の準備
+    @IBOutlet weak var addToTodoTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
+        addToTodoTextField.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return todos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = todos[indexPath.row]
         return cell
     }
+    //keybordのReturnが押された時に値を配列に追加してセルに表示する
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()//キーボードを閉じて
+        todos.append(addToTodoTextField.text!)//addToTodoTextField.textの値を入れる
+        tableView.reloadData()//セルに表示して
+        addToTodoTextField.text! = ""//addToTodoTextFieldの値を空にして閉じると
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
     @IBAction func createButton(_ sender: UIButton) {
-        items.append("\(count)")
+        todos.append("\(count)")
         count += 1
         tableView.reloadData()
     }
     
     @IBAction func deleteButton(_ sender: UIButton) {
-        if items.isEmpty {
+        if todos.isEmpty {
             label.text = "セルが空ですよー"
             //tableView.reloadData()
         } else {
             count -= 1
-            items.removeLast()//remove要素がないとエラーが出る
+            todos.removeLast()//remove要素がないとエラーが出る
+            //items.remove(at: tableView.indexPath(for: <#T##UITableViewCell#>))
             tableView.reloadData()
         }
     }
@@ -53,7 +72,7 @@ class ViewController: UIViewController, UITableViewDataSource , UITableViewDeleg
 //    }
     
     @IBAction func allDeleteButton(_ sender: UIButton) {
-        items.removeAll()
+        todos.removeAll()
         tableView.reloadData()
     }
     
